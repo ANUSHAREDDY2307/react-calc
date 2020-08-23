5762de9521e80540efc22fa4adb4c1634d77c278
anusha <anushareddy2307@gmail.com>
2020-08-23 16:52:37 +0530

    Updates

---
+++ precache-manifest.a5ae4269f98cbba58036b1ddcc047ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39e0fa9703399b7049d1e04326ab087",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "bce8bf78764ea4d89a49",
+    "url": "/react-calc/static/css/main.a59d3e2f.chunk.css"
+  },
+  {
+    "revision": "17640ae6e5e39d38dc2e",
+    "url": "/react-calc/static/js/2.f5ecc86c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc/static/js/2.f5ecc86c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce8bf78764ea4d89a49",
+    "url": "/react-calc/static/js/main.8ef81ede.chunk.js"
+  },
+  {
+    "revision": "316ae6202d6dd97efa2a",
+    "url": "/react-calc/static/js/runtime-main.ef11baa1.js"
+  }
+]);
